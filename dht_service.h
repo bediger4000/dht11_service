944ec9d669144f10f87c07014b5659d2ff6686b1
Bruce Ediger <bediger8@gmail.com>
2022-01-27 19:14:00 -0700

    collect_data() prototype

--- dht_service.h
+++ dht_service.h
@@ -2,4 +2,5 @@ int read_dht11(int pin, float *temperature, float *humidity);
 char *timestamp(void);
 int ds18b20_temp(float *temperature);
 in_addr_t find_address(char *server);
+void collect_data(in_addr_t server, uint16_t network_endian_port, int report_interval);
 
